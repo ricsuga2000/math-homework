2ae598d9f608e18a53b1307ebb2ef0f487e14cdc
ricsuga2000 <ricsuga2000@gmx.com>
2025-03-07 09:56:27 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int result = pow(num1, num2);
+    cout << "The result is: " << result << endl;
+    return 0;
+}
